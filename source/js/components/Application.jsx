bd6b48609a50b598ae6175a42c50105e9a7487b9
yujuan <15809216911@163.com>
2016-11-04 11:45:41 +0800

    Complish the interaction with react state

--- source/js/components/Application.jsx
+++ source/js/components/Application.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 
 let Application = React.createClass({
+  getInitialState: function () {
+    return {
+      showOptions: false
+    };
+  },
+
   render: function () {
+    let options;
+    if (this.state.showOptions) {
+      options = (
+          <ul className="options">
+            <li>apple</li>
+            <li>banana</li>
+            <li>pear</li>
+          </ul>
+      )
+    }
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <h1>React.js Application</h1>
-          </div>
+        <div className="dropdown" onClick={this.handleClick}>
+            <button>Choose a fruit</button>{options}
         </div>
-      </div>
-    );
+    )
+  },
+  handleClick: function () {
+    this.setState({
+      showOptions: true
+    });
   }
+
 });
 
 export default Application;
